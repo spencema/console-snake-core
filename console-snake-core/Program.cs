9ec30d5d20f98bd2e5d7e325dff0a5b6d602df83
Matthew Spencer <matthewspencer1991@gmail.com>
2018-11-22 22:18:10 +0000

    Generated project.

---
+++ console-snake-core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace console_snake_core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
